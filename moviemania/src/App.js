dda154c5046dcd372d566b6941f3eb68f3d17643
Ankur Verma <ankurverma7619@gmail.com>
2024-04-07 21:09:27 +0530

    Setting up project

---
+++ moviemania/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-blue-500 text-3xl font-bold underline">
+    MovieMania
+  </h1>
+    </div>
+  );
+}
+
+export default App;
