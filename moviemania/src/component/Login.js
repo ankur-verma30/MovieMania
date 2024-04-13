65b5af84086a1a4e11c029b8018a687c3e7d976f
Ankur Verma <ankurverma7619@gmail.com>
2024-04-14 00:51:29 +0530

    Encrypted Password

--- moviemania/src/component/Login.js
+++ moviemania/src/component/Login.js
@@ -1,14 +1,60 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { TailSpin } from "react-loader-spinner";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import swal from "sweetalert";
+import { where, query, getDocs } from "firebase/firestore";
+import { usersRef } from "../firebase/firebase";
+import { AppState } from "../App";
+import bcrypt from 'bcryptjs'
 
 const Login = () => {
+  const navigate = useNavigate();
+  const useAppState = useContext(AppState);
   const [form, setForm] = useState({
     mobile: "",
     password: "",
-
   })
   const [loading, setLoading] = useState(false)
+
+  const login = async () => {
+    setLoading(true);
+    try {
+      const quer = query(usersRef, where('mobile', '==', form.mobile))
+      const querySnapshot = await getDocs(quer);
+
+      querySnapshot.forEach((doc) => {
+        const _data = doc.data();
+        const isUser = bcrypt.compareSync(form.password, _data.password);
+        if (isUser) {
+          useAppState.setLogin(true);
+          useAppState.setUserName(_data.name);
+          swal({
+            title: "Logged In",
+            icon: "success",
+            buttons: false,
+            timer: 3000
+          })
+          navigate('/')
+        } else {
+          swal({
+            title: "Invalid Credentials",
+            icon: "error",
+            buttons: false,
+            timer: 3000
+          })
+        }
+      })
+    } catch (error) {
+      swal({
+        title: error.message,
+        icon: "error",
+        buttons: false,
+        timer: 3000
+      })
+    }
+    setLoading(false);
+  }
+
   return (
     <div className="w-full flex flex-col mt-8 items-center">
       <h1 className="text-xl font-bold">Login</h1>
@@ -25,7 +71,7 @@ const Login = () => {
       <div class="p-2 w-full md:w-1/3">
         <div class="relative">
           <label for="message" class="leading-7 text-sm text-gray-300">Password</label>
-          <input id="message" name="message"
+          <input type={'password'} id="message" name="message"
             value={form.password} onChange={(e) => setForm({
               ...form, password: e.target.value
             })}
@@ -33,7 +79,7 @@ const Login = () => {
         </div>
       </div>
       <div class="p-2 w-full">
-        <button class="flex mx-auto text-white bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg text-bold">{loading ? <TailSpin height={25} color="white" /> : 'Login'}</button>
+        <button onClick={login} class="flex mx-auto text-white bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg text-bold">{loading ? <TailSpin height={25} color="white" /> : 'Login'}</button>
       </div>
       <div className="">
         <p className="text-lg">Do not have account? <Link to={'/signup'}><span className="text-blue-500 text-bold"> Sign Up</span></Link></p>
